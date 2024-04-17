915c1342946102dc231a8c5e81773a76dd73b8fe
Nithya1234567890 <nithyamunagapati@gmai.com>
2024-04-17 10:47:41 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter,createRoutesFromElements , Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Categories from './components/Categories';
 import Contact from './components/Contact';
 
-const router=createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about/' element={<About/>}/>
-      <Route path='categories/' element={<Categories/>}/>
-      <Route path='contact/' element={<Contact/>}/>
-      </Route>
-  )
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route exact path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
